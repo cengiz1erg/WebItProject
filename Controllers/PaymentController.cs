5c0bbe5474b5b6f535c9a4a322359ef8cef69d97
Cengiz Ergün <cengiz1ergun@gmail.com>
2022-01-23 21:07:04 +0300

    Ödeme tamamlandı.

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebItProject.Extensions;
+using WebItProject.Models.Payment;
 using WebItProject.Services;
+using WebItProject.ViewModels;
 
 namespace WebItProject.Controllers
 {
@@ -19,7 +24,7 @@ namespace WebItProject.Controllers
 
             return View();
         }      
-        
+
         //[Authorize]
         [HttpPost]
         public IActionResult CheckInstallment(string binNumber)
@@ -30,8 +35,44 @@ namespace WebItProject.Controllers
                     Message = "Bad req."
                 });
 
-            var result = _paymentService.CheckInstallments(binNumber, 1000);
+            var result = _paymentService.CheckInstallments(binNumber, 90);
             return Ok(result);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Index(PaymentViewModel model)
+        {
+            var paymentModel = new PaymentModel()
+            {
+                Installment = model.Installment,
+                Address = new AddressModel(),
+                BasketList = new List<BasketModel>(),
+                Customer = new CustomerModel(),
+                CardModel = model.CardModel,
+                Price = 90,
+                UserId = HttpContext.GetUserId(),
+                Ip = Request.HttpContext.Connection.RemoteIpAddress?.ToString()
+            };
+
+            var installmentInfo = _paymentService.CheckInstallments(paymentModel.CardModel.CardNumber.Substring(0, 6), paymentModel.Price);
+
+            var installmentNumber =
+                installmentInfo.InstallmentPrices.FirstOrDefault(x => x.InstallmentNumber == model.Installment);
+
+            // if (installmentNumber != null)
+            // {
+            //     paymentModel.PaidPrice = decimal.Parse(installmentNumber.TotalPrice);
+            // }
+            // else
+            // {
+            //     paymentModel.PaidPrice = decimal.Parse(installmentInfo.InstallmentPrices[0].TotalPrice);
+            // }
+
+            paymentModel.PaidPrice = decimal.Parse(installmentNumber != null ? installmentNumber.TotalPrice : installmentInfo.InstallmentPrices[0].TotalPrice);
+
+            var result = _paymentService.Pay(paymentModel);
+            return View();
         }
     }
 }
--- Services/IyzicoPaymentService.cs
+++ Services/IyzicoPaymentService.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using AutoMapper;
 using Iyzipay.Model;
 using Iyzipay.Request;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using MUsefulMethods;
+using WebItProject.Models.Identity;
 using WebItProject.Models.Payment;
 
 namespace WebItProject.Services
@@ -12,13 +15,15 @@ namespace WebItProject.Services
     public class IyzicoPaymentService : IPaymentService
     {
         private readonly IConfiguration _configuration;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly IyzicoPaymentOptions _options;
         private readonly IMapper _mapper;
 
-        public IyzicoPaymentService(IConfiguration configuration, IMapper mapper)
+        public IyzicoPaymentService(IConfiguration configuration, IMapper mapper, UserManager<ApplicationUser> userManager)
         {
             _mapper = mapper;
             _configuration = configuration;
+            _userManager = userManager;
             var section = _configuration.GetSection(IyzicoPaymentOptions.Key);
             _options = new IyzicoPaymentOptions()
             {
@@ -31,13 +36,78 @@ namespace WebItProject.Services
 
         public PaymentResponseModel Pay(PaymentModel model)
         {
-            return null;
+            var request = this.InitialPaymentRequest(model);
+            var payment = Payment.Create(request, _options);          
+            return _mapper.Map<PaymentResponseModel>(payment);
+        }
+
+        private CreatePaymentRequest InitialPaymentRequest(PaymentModel model)
+        {
+            var paymentRequest = new CreatePaymentRequest
+            {
+                Installment = model.Installment,
+                Locale = Locale.TR.ToString(),
+                ConversationId = GenerateConversationId(),
+                Price = model.Price.ToString(new CultureInfo("en-US")),
+                PaidPrice = model.PaidPrice.ToString(new CultureInfo("en-US")),
+                Currency = Currency.TRY.ToString(),
+                BasketId = StringHelpers.GenerateUniqueCode(),
+                PaymentChannel = PaymentChannel.WEB.ToString(),
+                PaymentGroup = PaymentGroup.SUBSCRIPTION.ToString()
+            };
+
+            paymentRequest.PaymentCard = _mapper.Map<PaymentCard>(model.CardModel);
+
+            var user = _userManager.FindByIdAsync(model.UserId).Result;
+
+            var buyer = new Buyer
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Surname = user.Surname,
+                GsmNumber = user.PhoneNumber,
+                Email = user.Email,
+                IdentityNumber = "11111111110",
+                LastLoginDate = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}",
+                RegistrationDate = $"{user.CreatedDate:yyyy-MM-dd HH:mm:ss}",
+                RegistrationAddress = "Cihannuma Mah. Barbaros Bulvarı No:9 Beşiktaş",
+                Ip = model.Ip,
+                City = "Istanbul",
+                Country = "Turkey",
+                ZipCode = "34732"
+            };
+            paymentRequest.Buyer = buyer;
+
+            var billingAddress = new Address
+            {
+                ContactName = $"{user.Name} {user.Surname}",
+                City = "Istanbul",
+                Country = "Turkey",
+                Description = "Cihannuma Mah. Barbaros Bulvarı No:9 Beşiktaş",
+                ZipCode = "34742"
+            };
+            paymentRequest.BillingAddress = billingAddress;
+
+            var basketItems = new List<BasketItem>();
+
+            var firstBasketItem = new BasketItem
+            {
+                Id = "BI101",
+                Name = "Binocular",
+                Category1 = "Collectibles",
+                Category2 = "Accessories",
+                ItemType = BasketItemType.VIRTUAL.ToString(),
+                Price = model.Price.ToString(new CultureInfo("en-US"))
+            };
+            basketItems.Add(firstBasketItem);
+            paymentRequest.BasketItems = basketItems;
+
+            return paymentRequest;
         }
 
         public InstallmentModel CheckInstallments(string binNumber, decimal price)
         {
             var conversationId = GenerateConversationId();
-
             var request = new RetrieveInstallmentInfoRequest
             {
                 Locale = Locale.TR.ToString(),
@@ -47,7 +117,6 @@ namespace WebItProject.Services
             };
 
             var result = InstallmentInfo.Retrieve(request, _options);
-
             if (result.Status == "failure")
             {
                 throw new Exception(result.ErrorMessage);
@@ -58,7 +127,8 @@ namespace WebItProject.Services
                 throw new Exception("Hatalı istek oluturuldu");
             }
 
-            InstallmentModel resultModel = _mapper.Map<InstallmentModel>(result.InstallmentDetails[0]);
+            var resultModel = _mapper.Map<InstallmentModel>(result.InstallmentDetails[0]);
+
             return resultModel;
         }
 
