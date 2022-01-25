0e0f2d3025f4594d7807b8cfd215c6f49ebc0023
Cengiz Ergün <cengiz1ergun@gmail.com>
2022-01-25 15:40:58 +0300

    Load with DataSourceLoadOptions

--- Areas/Admin/Controllers/UserApiController.cs
+++ Areas/Admin/Controllers/UserApiController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DevExtreme.AspNet.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebItProject.Extensions;
 using WebItProject.Models.Identity;
 using WebItProject.ViewModels;
 
@@ -23,15 +25,17 @@ namespace WebItProject.Areas.Admin.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetUsers()
+        public IActionResult GetUsers(DataSourceLoadOptions loadOptions)
         {
-            var users = _userManager.Users.OrderBy(user => user.CreatedDate).ToList();
+            //var users = _userManager.Users.OrderBy(user => user.CreatedDate).ToList();
+            var data = _userManager.Users;
 
             //return Ok(users);
-            return Ok(new JsonResponseViewModel()
-            {
-                Data = users
-            });
+            //return Ok(new JsonResponseViewModel()
+            //{
+            //    Data = users
+            //});
+            return Ok(DataSourceLoader.Load(data, loadOptions));
         }
 
         [HttpGet]
